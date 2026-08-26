2f831241fecad08d42c5158ba0126d8d8ea658fb
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-08-26 03:46:07 -0600

    Practice 1 completed

---
+++ chapter_5_dictionaries.py
@@ -0,0 +1,10 @@
+"""
+Practice 1
+Create a dictionary called student with these keys and values:
+'name': your name
+'age': your age
+'course': "Python"
+Then print the value of 'name'.
+"""
+student = {'name': 'Evans Coe', 'age':'33', 'course':'Python'}
+print(student)
