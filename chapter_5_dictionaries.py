b5ec766deb4f3ff75191e0ce708135f3ff109848
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-08-31 11:01:50 -0600

    Practice 7 completed

--- chapter_5_dictionaries.py
+++ chapter_5_dictionaries.py
@@ -61,3 +61,20 @@ inventory['apple'] += 5
 print("number of apples: " + str(inventory['apple']))
 #3
 print(inventory)
+"""
+_____________________________________
+Practice 7
+Create an empty dictionary called letter_count.
+Loop through the string "programming" and count how many times each letter appears using setdefault().
+Then print the dictionary."""
+
+s = "programming"
+letter_count = {}
+
+for i in s:
+    # 1. Check if letter 'i' is in the dictionary, if not, set its default value to 0, then add 1
+    letter_count.setdefault(i, 0)
+    letter_count[i] += 1
+
+# 2. Print the final dictionary outside the loop
+print(letter_count)
