478e8feeb922814d4449316a00303aff31d6160e
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-09-01 18:29:47 -0600

    Chapter 6 practice 1 solved

---
+++ chapter_6_Manipulating_Strings.py
@@ -0,0 +1,12 @@
+"""
+Practice 1
+Ask the user for their full name.
+Then print:
+The name in uppercase
+The name in lowercase
+How many characters it has (including spaces)
+"""
+name = input("What is your full name?: ")
+print(name.upper())
+print(name.lower())
+print(len(name))
