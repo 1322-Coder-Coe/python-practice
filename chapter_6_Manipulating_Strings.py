97c3d720c271d029d5af21b11f6ec34cf6d5ad87
1322-Coder-Coe <amazon.ducalian007@gmail.com>
2026-09-01 20:00:08 -0600

    chapter 6 practice 4 solved

--- chapter_6_Manipulating_Strings.py
+++ chapter_6_Manipulating_Strings.py
@@ -5,13 +5,13 @@ Then print:
 The name in uppercase
 The name in lowercase
 How many characters it has (including spaces)
-"""
+
 name = input("What is your full name?: ")
 print(name.upper())
 print(name.lower())
 print(len(name))
 
-"""
+
 Practice 2
 Given this string:
 message = "  Hello World  "
@@ -19,18 +19,32 @@ Do the following:
 Remove the extra spaces from both ends
 Replace "World" with "Python"
 Print the final result
-"""
+
 message = "  Hello World  "
 message = message.strip().replace("World", "Python")
 print(message)
-"""
+
 Practice 3
 Create this list:
 words = ["I", "love", "Python"]
 Join the words into one sentence separated by spaces, so the result is:
 I love Python
 Print the final sentence.
-"""
+
 words = ["I", "love", "Python"]
 words= " ".join(words)
 print(words)
+
+Practice 4
+Ask the user to enter a password.
+Check if the password:
+Is at least 8 characters long
+Contains only letters and numbers (use .isalnum())
+Print "Strong password" if both conditions are true, otherwise print "Weak password".
+"""
+password= input("Enter Password: ")
+
+if len(password) >= 8 and password.isalnum():
+    print("Strong password")
+else:
+    print("Weak password")
